f6dd5b7982a41cc86da97bfe47a3410852395340
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2025-04-25 19:56:30 +0100

    saved

--- src/pages/FixedDeposit/AddFixedDeposit.jsx
+++ src/pages/FixedDeposit/AddFixedDeposit.jsx
@@ -16,6 +16,8 @@ const CreateFixedDeposit = () => {
     const [isLoading, setisLoading] = useState(false);
     const token = localStorage.getItem('token')
     const email = localStorage.getItem('email')
+    const [product, setProduct] = useState([])
+    const [tenure, setTenure] = useState([])
 
     const navigate = useNavigate()
 
@@ -32,11 +34,45 @@ const CreateFixedDeposit = () => {
     };
 
 
+    useEffect(() => {
+        const api1 = axios.get(`${middleware}loan/tenure`, config)
+        const api2 = axios.get(`${middleware}loan/products/all`, config)
+
+        setisLoading(true)
+
+        Promise.all([api1, api2]).then(([response1, response2]) => {
+            setTenure(response1.data.data)
+            setProduct(response2.data.data)
+        }).catch((e) => {
+
+
+
+            if (e.response.data.responseMessage === 'Invalid/Expired Token' || e.response.data.responseMessage === 'Invalid Token' || e.response.data.responseMessage === 'Login Token Expired') {
+                toast.error(e.response.data.responseMessage)
+                navigate('/auth/login')
+                localStorage.clear()
+            }
+            else if (e.response.data.responseMessage === 'Insufficient permission') {
+                toast.error(e.response.data.responseMessage)
+                navigate('/')
+            }
+            else {
+                toast.error(e.response.data.responseMessage)
+            }
+        }).finally(() => {
+            setisLoading(false)
+        })
+    }, [])
+
+
+
+
     const formik = useFormik({
         initialValues: {
+            // productCode:'',
             fixedDepositType: '',
             description: '',
-            tenure: 0,
+            tenure: '',
             minAmount: 0,
             maxAmount: 0,
             rate: 0,
@@ -46,16 +82,16 @@ const CreateFixedDeposit = () => {
 
         },
         validationSchema: fixedDeposit,
-        onSubmit: (values,{resetForm}) => {
+        onSubmit: (values, { resetForm }) => {
             setisLoading(true)
             axios.post(`${middleware}fixed-deposit-setup`, values, config)
                 .then((res) => {
                     toast.success(`${res.data.responseMessage}`)
-                     resetForm()
-                    
+                    resetForm()
+
                 })
                 .catch((e) => {
-                    
+
 
                     if (e.response.data.responseMessage === 'Invalid/Expired Token' || e.response.data.responseMessage === 'Invalid Token' || e.response.data.responseMessage === 'Login Token Expired') {
                         toast.error(e.response.data.responseMessage)
@@ -77,124 +113,150 @@ const CreateFixedDeposit = () => {
 
         }
     })
-  return (
-    <div className=" bg-gray-100 ">
-    {isLoading && (
-        <div className="fixed bg-black/[0.6] h-screen w-screen z-50 left-0 top-0 items-center flex justify-center">
-            {" "}
-            <PulseLoader speedMultiplier={0.9} color="#fff" size={20} />
-        </div>
-    )}
-    <div className="max mx-auto bg-white rounded-lg shadow-xl overflow-hidden">
-        <div className="px-6 py-4">
-            <button onClick={() => navigate(-1)} className="mb-6 flex items-center text-[#072D56] transition-colors">
-                <BiArrowBack className="mr-2" />
-                Back
-            </button>
-            <h2 className="text-2xl font-bold text-gray-900 mb-6">Create Fixed Deposit</h2>
-            <form className="space-y-4" onSubmit={formik.handleSubmit}>
-                <div className='grid l:grid-cols-3 grid-cols-2 gap-6'>
-
-                    <InputField2
-                        label={`Fixed Deposit Type`}
-                        name={`fixedDepositType`}
-                        value={formik.values.fixedDepositType}
-                        onChange={formik.handleChange}
-                        error={formik.touched.fixedDepositType && formik.errors.fixedDepositType}
-                        errorText={formik.errors.fixedDepositType}
-
-                    />
-
-                    <InputField2
-                        label={`Description`}
-                        name={`description`}
-                        value={formik.values.description}
-                        onChange={formik.handleChange}
-                        error={formik.touched.description && formik.errors.description}
-                        errorText={formik.errors.description}
-
-                    />
-
-
-                    <InputField2
-                        label={`Tenure`}
-                        name={`tenure`}
-                        value={formik.values.tenure}
-                        type={'number'}
-                        onChange={formik.handleChange}
-                        error={formik.touched.tenure && formik.errors.tenure}
-                        errorText={formik.errors.tenure}
-                    />
-
-                    <InputField2
-                        label={`Min Amount`}
-                        name={`minAmount`}
-                        value={formik.values.minAmount}
-                        type={'number'}
-                        onChange={formik.handleChange}
-                        error={formik.touched.minAmount && formik.errors.minAmount}
-                        errorText={formik.errors.minAmount}
-                    />
-
-                    <InputField2
-                        label={`Max Amount`}
-                        name={`maxAmount`}
-                        value={formik.values.maxAmount}
-                        type={'number'}
-                        onChange={formik.handleChange}
-                        error={formik.touched.maxAmount && formik.errors.maxAmount}
-                        errorText={formik.errors.maxAmount}
-                    />
-
-
-                    <InputField2
-                        label={`Rate`}
-                        name={`rate`}
-                        value={formik.values.rate}
-                        type={'number'}
-                        onChange={formik.handleChange}
-                        error={formik.touched.rate && formik.errors.rate}
-                        errorText={formik.errors.rate}
-                    />
-
-                    <InputField2
-                        label={`Tax`}
-                        name={`tax`}
-                        value={formik.values.tax}
-                        type={'number'}
-                        onChange={formik.handleChange}
-                        error={formik.touched.tax && formik.errors.tax}
-                        errorText={formik.errors.tax}
-                    />
-
-                   
-
-
-
-
-
+    return (
+        <div className=" bg-gray-100 ">
+            {isLoading && (
+                <div className="fixed bg-black/[0.6] h-screen w-screen z-50 left-0 top-0 items-center flex justify-center">
+                    {" "}
+                    <PulseLoader speedMultiplier={0.9} color="#fff" size={20} />
                 </div>
-                <div className='flex items-center mt-6'>
-                        <div className='flex items-center justify-between '>
-                            <div className='flex items-center mr-4'>
-                                <label className='mr-2'>Tax Deductible</label>
-                                <input className='w-5 h-5' type='checkbox' name={`isTaxDeductible`} checked={formik.values.isTaxDeductible} onChange={formik.handleChange} />
-                            </div>
-                            <div className='flex items-center'>
-                                <label className='mr-2'>Available</label>
-                                <input className='w-5 h-5' type='checkbox' name={`isAvailable`} checked={formik.values.isAvailable} onChange={formik.handleChange} />
-                            </div>
+            )}
+            <div className="max mx-auto bg-white rounded-lg shadow-xl overflow-hidden">
+                <div className="px-6 py-4">
+                    <button onClick={() => navigate(-1)} className="mb-6 flex items-center text-[#072D56] transition-colors">
+                        <BiArrowBack className="mr-2" />
+                        Back
+                    </button>
+                    <h2 className="text-2xl font-bold text-gray-900 mb-6">Create Fixed Deposit</h2>
+                    <form className="space-y-4" onSubmit={formik.handleSubmit}>
+                        <div className='grid l:grid-cols-3 grid-cols-2 gap-6'>
+
+                            {/* <SelectField
+                                label="Product Code"
+                                name={`productCode`}
+                                options={product.map(item => ({
+                                    value: item.productCode,
+                                    label: item.productName
+                                }))}
+                                value={formik.values.productCode}
+                                onChange={formik.handleChange}
+                                error={formik.touched.productCode && formik.errors.productCode}
+                                errorText={formik.errors.productCode}
+                            />
+                            <SelectField
+                                label="Tenure"
+                                name="tenure"
+                                options={tenure.map(item => ({
+                                    value: item.tenure, // or whatever field holds the value
+                                    label: item.description // or whatever field holds the display text
+                                }))}
+                                value={formik.values.tenure}
+                                onChange={formik.handleChange}
+                                error={formik.touched.tenure && formik.errors.tenure}
+                                errorText={formik.errors.tenure}
+                            /> */}
+
+                            <InputField2
+                                label={`Fixed Deposit Type`}
+                                name={`fixedDepositType`}
+                                value={formik.values.fixedDepositType}
+                                onChange={formik.handleChange}
+                                error={formik.touched.fixedDepositType && formik.errors.fixedDepositType}
+                                errorText={formik.errors.fixedDepositType}
+
+                            />
+
+                            <InputField2
+                                label={`Description`}
+                                name={`description`}
+                                value={formik.values.description}
+                                onChange={formik.handleChange}
+                                error={formik.touched.description && formik.errors.description}
+                                errorText={formik.errors.description}
+
+                            />
+
+
+                            <InputField2
+                                label={`Tenure`}
+                                name={`tenure`}
+                                value={formik.values.tenure}
+                                type={'number'}
+                                onChange={formik.handleChange}
+                                error={formik.touched.tenure && formik.errors.tenure}
+                                errorText={formik.errors.tenure}
+                            />
+
+                            <InputField2
+                                label={`Min Amount`}
+                                name={`minAmount`}
+                                value={formik.values.minAmount}
+                                type={'number'}
+                                onChange={formik.handleChange}
+                                error={formik.touched.minAmount && formik.errors.minAmount}
+                                errorText={formik.errors.minAmount}
+                            />
+
+                            <InputField2
+                                label={`Max Amount`}
+                                name={`maxAmount`}
+                                value={formik.values.maxAmount}
+                                type={'number'}
+                                onChange={formik.handleChange}
+                                error={formik.touched.maxAmount && formik.errors.maxAmount}
+                                errorText={formik.errors.maxAmount}
+                            />
+
+
+                            <InputField2
+                                label={`Rate`}
+                                name={`rate`}
+                                value={formik.values.rate}
+                                type={'number'}
+                                onChange={formik.handleChange}
+                                error={formik.touched.rate && formik.errors.rate}
+                                errorText={formik.errors.rate}
+                            />
+
+                            <InputField2
+                                label={`Tax`}
+                                name={`tax`}
+                                value={formik.values.tax}
+                                type={'number'}
+                                onChange={formik.handleChange}
+                                error={formik.touched.tax && formik.errors.tax}
+                                errorText={formik.errors.tax}
+                            />
+
+
+
+
+
+
+
                         </div>
+                        <div className='flex items-center mt-6'>
+                            <div className='flex items-center justify-between '>
+                                <div className='flex items-center mr-4'>
+                                    <label className='mr-2'>Tax Deductible</label>
+                                    <input className='w-5 h-5' type='checkbox' name={`isTaxDeductible`} checked={formik.values.isTaxDeductible} onChange={formik.handleChange} />
+                                </div>
+                                <div className='flex items-center'>
+                                    <label className='mr-2'>Available</label>
+                                    <input className='w-5 h-5' type='checkbox' name={`isAvailable`} checked={formik.values.isAvailable} onChange={formik.handleChange} />
+                                </div>
+                            </div>
 
-                    </div>
-                <button type='submit' className="text-white btn w-full bg-[#072D56] rounded-[10px] px-5 py-2"  > {isLoading ? 'loading....' : 'Create Fixed Deposit'}</button>
-            </form>
+                        </div>
+                        <button type='submit' onClick={() => console.log('Button clicked', formik.errors, formik.isValid, formik.dirty)}
+ className="text-white btn w-full bg-[#072D56] rounded-[10px] px-5 py-2"  > {isLoading ? 'loading....' : 'Create Fixed Deposit'}</button>
+                    </form>
+                </div>
+
+
+            </div>
         </div>
-
-
-    </div>
-</div>
-  )
+    )
 }
 
 export default CreateFixedDeposit
